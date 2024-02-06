bf50ea8817a737c668c5fe8acb7f4ff58cad1cdf
AryaJulianda <official.aryajulianda@gmail.com>
2024-02-06 23:02:29 +0700

    feat(update-prompt) : create update-prompt page and api

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,10 +1,14 @@
 "use client";
+
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -14,20 +18,20 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   };
   return (
     <div className="prompt_card">
-      <div className="flex justify-between items-start gap-5">
-        <div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
+      <div className="flex items-start justify-between gap-5">
+        <div className="flex items-center justify-start flex-1 gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
             alt={"user_image"}
             width={40}
             height={40}
-            className="rounded-full object-contain"
+            className="object-contain rounded-full"
           />
           <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">
+            <h3 className="font-semibold text-gray-900 font-satoshi">
               {post.creator.username}
             </h3>
-            <p className="font-inter text-sm text-gray-500">
+            <p className="text-sm text-gray-500 font-inter">
               {post.creator.email}
             </p>
           </div>
@@ -47,13 +51,30 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         </div>
       </div>
 
-      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p className="my-4 text-sm text-gray-700 font-satoshi">{post.prompt}</p>
       <p
-        className="font-inter text-sm blue_gradient cursor-pointer"
+        className="text-sm cursor-pointer font-inter blue_gradient"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
         #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="gap-4 pt-3 mt-5 border-t border-gray-100 flex-center">
+          <p
+            className="text-sm cursor-pointer font-inter green_gradient"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="text-sm cursor-pointer font-inter orange_gradient"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   email: {
     type: String,
-    unique: [true, "Email Already Exist!"],
-    required: [true, "Email is Required!"],
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
   },
   username: {
     type: String,
@@ -20,4 +20,5 @@ const UserSchema = new Schema({
 });
 
 const User = models.User || model("User", UserSchema);
+
 export default User;
